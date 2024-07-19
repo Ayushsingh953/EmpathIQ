4f4961a2ebc4321eda50abc126357f9cddf84160
ayushsingh953 <ayushsingh35323@gmail.com>
2024-07-19 16:42:34 +0530

    user login

--- screens/auth/Login.js
+++ screens/auth/Login.js
@@ -4,6 +4,11 @@ import { Dimensions, Image, Pressable, ScrollView, StyleSheet, Text, View } from
 import Input from "../../components/input";
 import CustomButton from "../../components/customButton";
 import { useState } from "react";
+import LoadingModal from "../../components/loadingModal";
+import Toast from "react-native-toast-message";
+import { FIREBASE_AUTH } from "../../firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import showToast from "../../utils/showToast";
 
 const {width,height} = Dimensions.get("screen");
 
@@ -11,10 +16,36 @@ export default function Login({navigation}){
 
     const[email,setEmail] = useState();
     const[password,setPassword] = useState();
+    const[loading,setLoading] = useState(false);
+
+    async function LoginHandler(){
+
+        if(!email || !password){
+          showToast("Alert","All fileds are required!","error","red");
+          return;
+        }
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const validEmail=emailPattern.test(email);
+        if(!validEmail){
+            showToast("Alert","Please provide valid email!","error","red");
+          return;
+        }
+        setLoading(true);
+        try{
+          const response = await signInWithEmailAndPassword(FIREBASE_AUTH,email,password);
+          console.log(response.user);
+          showToast("Success","Logged in!","success","green");
+        }catch(err){
+          console.log("Error loging in user ",err);
+          showToast("Error","Something went wrong!","error","red");
+          }
+        setLoading(false);
+      }
 
     return (
         <ScrollView contentContainerStyle={{width:width,height:height,alignItems:"center"}}>
         <StatusBar style="dark" />
+        <LoadingModal visible={loading} label="Logging you in..." />
          <LinearGradient colors={["#e2d5c6","#b4bdd9","#8a9ec4"]} style={StyleSheet.absoluteFillObject}/>
          <Image source={require("../../assets/login.png")} style={{width:width*0.9,height:height*0.4,marginTop:30}}/>
          <Text style={{fontSize:40,color:"white",fontWeight:900}}>Login</Text>
@@ -30,7 +61,7 @@ export default function Login({navigation}){
             <Input label="EMAIL :" type="email" placeholder="Enter email" value={email} onChangeText={setEmail}/>
             <Input label="PASSWORD :" type="default" secure={true} placeholder="Enter password" value={password} onChangeText={setPassword} />
          </View>
-         <CustomButton label="LOGIN" styles={{button:styles.button,text:styles.text}} />
+         <CustomButton label="LOGIN" styles={{button:styles.button,text:styles.text}} onPress={LoginHandler}/>
          </ScrollView>
     )
 }
--- screens/authenticated/screen3.js
+++ screens/authenticated/screen3.js
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
+import { FIREBASE_AUTH } from "../../firebaseConfig";
 
 export default function Screen3(){
     return (
         <View style={{flex:1,alignItems:"center",justifyContent:"center"}}>
             <Text>Profile Screen</Text>
+            <Button title="logout" onPress={()=>{
+                FIREBASE_AUTH.signOut();
+            }}/>
         </View>
     )
 }
